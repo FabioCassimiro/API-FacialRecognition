dc8bf73a31fa7bb149a8d58fa1bfdaec33a963ad
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-04 21:54:23 -0300

    Adicionado metodo para a comparação de faces

--- app/controllers/EncodesFace.py
+++ app/controllers/EncodesFace.py
@@ -10,33 +10,48 @@ def adiciona_rostos_conhecidos(arquivo):
 
 
 
-rosto_conhecidos = [
-    adiciona_rostos_conhecidos("teste.jpeg"),
-    adiciona_rostos_conhecidos("teste1.jpeg"),
-    adiciona_rostos_conhecidos("teste2.jpeg")
-]
+# rosto_conhecidos = [
+#     adiciona_rostos_conhecidos("teste.jpeg"),
+#     adiciona_rostos_conhecidos("teste1.jpeg"),
+#     adiciona_rostos_conhecidos("teste2.jpeg")
+# ]
 
-nomes_rostos_conhecidos = [
-    "Daniel",
-    "Walisson",
-    "Daniele"
-]
+# nomes_rostos_conhecidos = [
+#     "Daniel",
+#     "Walisson",
+#     "Daniele"
+# ]
+
+def compara(name):
+
+    rosto_conhecidos = [
+    adiciona_rostos_conhecidos("Daniel.jpeg"),
+    adiciona_rostos_conhecidos("Walisson.jpeg"),
+    adiciona_rostos_conhecidos("Daniele.jpeg"),
+    adiciona_rostos_conhecidos("Beatriz.jpeg")
+    ]
+
+    nomes_rostos_conhecidos = [
+        "Daniel",
+        "Walisson",
+        "Daniele",
+        "Beatriz"
+    ]
+
+    imagem_comparativa = fr.load_image_file(url_image + name)
+    local_rosto = fr.face_locations(imagem_comparativa)
+    rosto_codificado = fr.face_encodings(imagem_comparativa,local_rosto)
 
 
-imagem_comparativa = fr.load_image_file(url_image + "Daniel.jpeg")
-local_rosto = fr.face_locations(imagem_comparativa)
-rosto_codificado = fr.face_encodings(imagem_comparativa,local_rosto)
 
+    for (top, right, bottom, left), face_encoding in zip(local_rosto, rosto_codificado):
+        
+        comparacao = fr.compare_faces(rosto_conhecidos, face_encoding,0.5)
 
-
-for (top, right, bottom, left), face_encoding in zip(local_rosto, rosto_codificado):
-    
-    comparacao = fr.compare_faces(rosto_conhecidos, face_encoding,0.7)
-
-    name = "Desconhecido"
-    if True in comparacao:
-        melhor_comparacao = comparacao.index(True)
-        name = nomes_rostos_conhecidos[melhor_comparacao]
-        print(name)
-    else:
-        print("Não achei")
+        name = "Desconhecido"
+        if True in comparacao:
+            melhor_comparacao = comparacao.index(True)
+            name = nomes_rostos_conhecidos[melhor_comparacao]
+            return name
+        else:
+            return "Não conhecido"

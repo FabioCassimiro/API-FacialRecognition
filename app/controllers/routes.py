71f7b78345b8fcb08b517d14e4b373a619b2784c
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-04 21:55:43 -0300

    Adicionado a rota para recebimento de imagem

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,7 +1,20 @@
-from flask import request
 from app import app
+from flask import request
+from app.controllers.EncodesFace import compara
+import os
+from werkzeug.utils import secure_filename
+
+app.config['imgdir'] = "./face"
+
 
 @app.route("/")
-@app.route("/recognition")
+@app.route("/recognition",methods=['POST'])
 def recognition():
-    return "ok"
+    if request.files:
+        file =request.files['image']
+        filename = secure_filename(file.filename) # save file 
+        filepath = os.path.join(app.config['imgdir'], filename);
+        file.save(filepath)
+    
+    return compara(filename)
+    

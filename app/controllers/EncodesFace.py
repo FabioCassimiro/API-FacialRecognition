18dd1ce477e96b7efdf15b97b2d71b3bec953e0b
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-26 11:18:21 -0300

    Alteração da classe para consulta imagens de comparação no banco de dados

--- app/controllers/EncodesFace.py
+++ app/controllers/EncodesFace.py
@@ -1,42 +1,29 @@
 import face_recognition as fr
+from app.models.searchFaces import recognition_face 
 
-url_image = ".\\face\\"
+
+url_data_image = ".\\face\\"
+url_image = ".\\faces_request\\"
+
+rosto_usuario = []
+nome_usuario = []
+
+def facial_recognition(user,filename):
+    try:
+        recognition_face(user)
+        rosto_usuario.append(adiciona_rostos_conhecidos('imagemBanco.jpeg'))
+        nome_usuario.append(user)
+        return compara(filename)
+    except Exception as identifier:
+        print(identifier)
 
 
 def adiciona_rostos_conhecidos(arquivo):
-    imagem_banco = fr.load_image_file(url_image + arquivo)
+    imagem_banco = fr.load_image_file(url_data_image + arquivo)
     imagem_banco_encoding = fr.face_encodings(imagem_banco)[0]
     return imagem_banco_encoding
 
-
-
-# rosto_conhecidos = [
-#     adiciona_rostos_conhecidos("teste.jpeg"),
-#     adiciona_rostos_conhecidos("teste1.jpeg"),
-#     adiciona_rostos_conhecidos("teste2.jpeg")
-# ]
-
-# nomes_rostos_conhecidos = [
-#     "Daniel",
-#     "Walisson",
-#     "Daniele"
-# ]
-
 def compara(name):
-
-    rosto_conhecidos = [
-    adiciona_rostos_conhecidos("Daniel.jpeg"),
-    adiciona_rostos_conhecidos("Walisson.jpeg"),
-    adiciona_rostos_conhecidos("Daniele.jpeg"),
-    adiciona_rostos_conhecidos("Beatriz.jpeg")
-    ]
-
-    nomes_rostos_conhecidos = [
-        "Daniel",
-        "Walisson",
-        "Daniele",
-        "Beatriz"
-    ]
 
     imagem_comparativa = fr.load_image_file(url_image + name)
     local_rosto = fr.face_locations(imagem_comparativa)
@@ -46,12 +33,12 @@ def compara(name):
 
     for (top, right, bottom, left), face_encoding in zip(local_rosto, rosto_codificado):
         
-        comparacao = fr.compare_faces(rosto_conhecidos, face_encoding,0.5)
+        comparacao = fr.compare_faces(rosto_usuario, face_encoding,0.5)
 
         name = "Desconhecido"
         if True in comparacao:
             melhor_comparacao = comparacao.index(True)
-            name = nomes_rostos_conhecidos[melhor_comparacao]
-            return name
+            name = nome_usuario[melhor_comparacao]
+            return {'authenticated-face': True}
         else:
-            return "Não conhecido"
+            return {'authenticated-face': False}

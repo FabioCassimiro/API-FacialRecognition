ad9a169c2ca9b10af4c7fa82e5e9e1a506b9305f
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-30 20:48:37 -0300

    Resolução do problema de cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={
+    r'/*':{
+        "origins":"*"
+    }
+})
+
 app.config.from_object('config')
 
 from app.controllers import routes

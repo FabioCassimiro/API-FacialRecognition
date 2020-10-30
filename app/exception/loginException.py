3697b630a379de72c2bc411729a6bb11d3c6c77b
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-30 20:45:59 -0300

    Classe responsavel por exceções do programa

---
+++ app/exception/loginException.py
@@ -0,0 +1,18 @@
+class UserNotFound(Exception):
+    
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+        
+
+    def __str__(self):
+        return self.mensagem
+
+
+class FaceNotMatch(Exception):
+    
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+        
+
+    def __str__(self):
+        return self.mensagem

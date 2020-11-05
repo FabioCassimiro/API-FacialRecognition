eb002ac3923d3b42da99ebc74b58721f84b46258
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-11-05 17:19:56 -0300

    Refatoração  do codigo e alteração de dados do banco
    Refatoração de codigo, alteração de dados do banco de dados para variaveis especificadas no arquivo de configuração e remoção de metodo repetido

--- app/models/searchFaces.py
+++ app/models/searchFaces.py
@@ -1,15 +1,23 @@
+from app import app
+from app.models.images import convert_to_image
 import mysql.connector
-import base64
-import os
 
-cnx = mysql.connector.connect(user="root", database="recognition_system")
+cnx = mysql.connector.connect(
+    host = app.config['HOST'],
+    user = app.config['USERDB'],
+    database = app.config['DATABASE'],
+    password = app.config['PASSWORDDB']
+)
+
 consult = cnx.cursor()
 
+# Resposavel por buscar imagem do usuario no banco de dados e salvar
+# no caminho especificado 
 
 def recognition_face(username):
-        convert_to_image('./face/imagemBanco.jpeg',search_image(username))
+        convert_to_image(search_image(username),'./face/imagemBanco.jpeg')
 
-
+# busca os bytes da imagem salva no banco de dados do respectivo usuario.
 def search_image(username):
     query = "SELECT IMAGE FROM IMAGES WHERE USERNAME = %s"
 
@@ -20,14 +28,7 @@ def search_image(username):
                 username,
             )
         )
-        result = consult.fetchone()
-        return result[0]
-    except Exception as identifier:
-        print("Error:",identifier)
-
-
-def convert_to_image(url,base64image):
-
-    with open(url, 'wb') as convert:
-        convert.write(base64.b64decode((base64image)))
-        convert.close()
+        image_user = consult.fetchone()
+        return image_user[0]
+    except Exception as error:
+        print("Error: ", error)

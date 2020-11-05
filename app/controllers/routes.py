519425ee1bb82f55468fa37359b73b559d657a5c
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-11-05 17:20:52 -0300

    Alteração de response
    adicionado response para as situações de login

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,12 +1,13 @@
 from app import app
+from app.models import login 
 from flask import request
-from app.models import login
-import os
 
     
 
 
 @app.route("/facialrecognition", methods=['POST'])
 def facial_recognition_login():
-    result = login.valid_user(request.form)
-    return result,200, {'Content-Type': 'application/json'}
+    if login.valid_user(request.form):
+        return {'authenticated':True},200, {'Content-Type': 'application/json'}
+    else:
+        return {'authenticated':False},401, {'Content-Type': 'application/json'}
